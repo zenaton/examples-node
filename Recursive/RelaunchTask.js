a35f1a19f5d873ee10916720bb6abf725ff74e3a
Gilles Barbier <gilles@thefamily.co>
2018-01-31 18:02:24 +0100

    improve recursive example

--- Recursive/RelaunchTask.js
+++ Recursive/RelaunchTask.js
@@ -1,22 +1,17 @@
 require("dotenv").config({path: __dirname + "/../.env"});
-var {Task, Client, Workflow} = require("zenaton");
+var {Task, Workflow} = require("zenaton");
 
 module.exports = Task("RelaunchTask", {
   init(id) {
     this.id = id;
-
-    var app_id = process.env.ZENATON_APP_ID;
-    var api_token = process.env.ZENATON_API_TOKEN;
-    var app_env = process.env.ZENATON_APP_ENV;
-    Client.init(app_id, api_token, app_env);
   },
   handle(done) {
     // Get workflow class definition
     var RecursiveWorkflow = Workflow("RecursiveWorkflow");
 
     // launch new workflow
-    new RecursiveWorkflow(1 + this.id).dispatch();
-
-    done();
+    new RecursiveWorkflow(1 + this.id).dispatch()
+      .then( result => { done(null, result); })
+      .catch( error => { done(error, null); });
   }
 });
--- launch_recursive.js
+++ launch_recursive.js
@@ -1,4 +1,4 @@
 require("./client");
 var RecursiveWorkflow = require("./Recursive/RecursiveWorkflow");
 
-new RecursiveWorkflow(70).dispatch();
+new RecursiveWorkflow(0).dispatch();
