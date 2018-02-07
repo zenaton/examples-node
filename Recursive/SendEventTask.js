51f8d50d358b92a5c618e3479e7ce9e7f320232d
Gilles Barbier <gilles@thefamily.co>
2018-02-07 10:41:40 +0100

    Add Readme

--- Recursive/SendEventTask.js
+++ Recursive/SendEventTask.js
@@ -1,14 +1,9 @@
 require("dotenv").config({path: __dirname + "/../.env"});
-var {Task, Client, Workflow} = require("zenaton");
+var {Task, Workflow} = require("zenaton");
 
 module.exports = Task("SendEventTask", {
   init(id) {
     this.id = id;
-
-    var app_id = process.env.ZENATON_APP_ID;
-    var api_token = process.env.ZENATON_API_TOKEN;
-    var app_env = process.env.ZENATON_APP_ENV;
-    Client.init(app_id, api_token, app_env);
   },
   handle(done) {
     console.log("\nIteration: " + this.id);
