82d4edc2c386eb3e7a2145b15b695bab85342e21
Gilles Barbier <gilles@zenaton.com>
2019-08-19 20:08:43 +0200

    fix TaskA

--- Tasks/TaskA.js
+++ Tasks/TaskA.js
@@ -1,5 +1,4 @@
 const { task } = require("zenaton");
-const { dispatch } = require("./client");
 
 module.exports = task("TaskA", async function() {
   console.log("Task A starts");
@@ -7,8 +6,6 @@ module.exports = task("TaskA", async function() {
   await new Promise(resolve => {
     setTimeout(resolve, 3000);
   });
-
-  dispatch.workflow("SequentialWorkflow");
 
   console.log("Task A ends");
   return 0;
